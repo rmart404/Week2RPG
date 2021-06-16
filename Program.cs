f5aeed0591af720b7b5783c58000594013694cba
Ray Martinez <rmart404@stu.southtexascollege.edu>
2021-06-16 12:26:20 -0500

    Week 2 Done

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using RPG;
+
+Player Ray = new();
+
+Dragon Smaug = new();
+
+Goblin Nobody = new();
+
+Ogre Another = new();
+
+Console.WriteLine(Ray.hitPower);
+
+for (var i = 0; i < 3; i++)
+{
+  Ray.hit(Smaug);
+}
+
+Ray.hit(Nobody);
+
+
+Ray.levelUp();
+
+Ray.hit(Smaug);
+Ray.hit(Smaug);
+Ray.hit(Smaug);
+
+Ray.hit(Nobody);
+
+Ray.hit(Another);
+Ray.hit(Another);
